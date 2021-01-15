57d9816ba5de84c50fc020a1716349653242996f
Николай Сусоев <susoev.nikolay@mail.ru>
2021-01-15 21:39:03 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Explorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxFile = new System.Windows.Forms.TextBox();
+            this.btnPath = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelFolderNow = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelTimer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(354, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Запуск поиска";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(16, 170);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(760, 415);
+            this.treeView1.TabIndex = 1;
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(13, 36);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(324, 20);
+            this.textBoxPath.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Выберите стартовую дерикторию:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(452, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Введите шаблон имени файла:";
+            // 
+            // textBoxFile
+            // 
+            this.textBoxFile.Location = new System.Drawing.Point(452, 36);
+            this.textBoxFile.Name = "textBoxFile";
+            this.textBoxFile.Size = new System.Drawing.Size(324, 20);
+            this.textBoxFile.TabIndex = 4;
+            // 
+            // btnPath
+            // 
+            this.btnPath.Image = ((System.Drawing.Image)(resources.GetObject("btnPath.Image")));
+            this.btnPath.Location = new System.Drawing.Point(354, 13);
+            this.btnPath.Name = "btnPath";
+            this.btnPath.Size = new System.Drawing.Size(64, 64);
+            this.btnPath.TabIndex = 6;
+            this.btnPath.UseVisualStyleBackColor = true;
+            this.btnPath.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(172, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Папка в которой ведётся поиск:";
+            // 
+            // labelFolderNow
+            // 
+            this.labelFolderNow.AutoSize = true;
+            this.labelFolderNow.Location = new System.Drawing.Point(13, 127);
+            this.labelFolderNow.Name = "labelFolderNow";
+            this.labelFolderNow.Size = new System.Drawing.Size(209, 13);
+            this.labelFolderNow.TabIndex = 8;
+            this.labelFolderNow.Text = "Тут будет отображаться текущая папка";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelTimer
+            // 
+            this.labelTimer.AutoSize = true;
+            this.labelTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTimer.Location = new System.Drawing.Point(579, 87);
+            this.labelTimer.Name = "labelTimer";
+            this.labelTimer.Size = new System.Drawing.Size(0, 25);
+            this.labelTimer.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 587);
+            this.Controls.Add(this.labelTimer);
+            this.Controls.Add(this.labelFolderNow);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxFile;
+        private System.Windows.Forms.Button btnPath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelFolderNow;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelTimer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Explorer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 1000;
+            //timer1.Tick += timer1_Tick;
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.path = textBoxPath.Text;
+            Properties.Settings.Default.fileName = textBoxFile.Text;
+            Properties.Settings.Default.Save();
+
+        }
+        public int timeStart;
+        private void button2_Click(object sender, EventArgs e)//btnPath; УБРАТЬ ВЫБОР ФАЙЛА
+        {
+            OpenFileDialog choofdlog = new OpenFileDialog();
+            
+            if (choofdlog.ShowDialog() == DialogResult.OK)
+            {
+                string sFileName = choofdlog.FileName;
+                string directoryPath = Path.GetDirectoryName(sFileName);
+                textBoxPath.Text = directoryPath;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime tempDate = DateTime.Now;
+            int hourStart = tempDate.Hour;
+            int minStart = tempDate.Minute;
+            int secondsStart = tempDate.Second;
+            int time = ( secondsStart + minStart * 60 + hourStart * 3600 - timeStart);
+            int timeH = time / 3600;
+            int timeM = time / 60;
+            int timeS = time % 60;
+            string H,M,S;
+            if (timeH < 10)
+                H = "0"+(timeH).ToString();
+            else 
+                H = (timeH).ToString();
+            if (timeM < 10)
+            {
+                M = "0" + (timeM).ToString();
+            }
+            else
+                M = (timeM).ToString();
+            if (timeS < 10)
+            {
+                S = "0" + (timeS).ToString();
+            }
+            else
+                S = (timeS).ToString();
+            labelTimer.Text = H+":"+ M + ":" + S;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime localDate = DateTime.Now;
+            int hourStart = localDate.Hour;
+            int minStart = localDate.Minute;
+            int secondsStart = localDate.Second;
+            timeStart = secondsStart + minStart*60 + hourStart*3600;
+            timer1.Enabled = true;
+            timer1.Start();
+
+
+
+
+
+
+
+
+           
+
+        }
+    }
+}
